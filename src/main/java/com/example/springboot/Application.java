4b82fbf5512bd4c73d698c53bd2dc2b672e18e84
Ankur Gupta <ankurdotcom@gmail.com>
2020-11-21 16:47:35 +0530

    Added some more intial files

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Om Namah Shivoy");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/java8/HelloControllerTest.java
@@ -0,0 +1,79 @@
+package java8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HelloControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void testIndex() {
+		////fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testObject() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetClass() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testHashCode() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testEquals() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testClone() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testToString() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotify() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testNotifyAll() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWait() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLong() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testWaitLongInt() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testFinalize() {
+		//fail("Not yet implemented"); // TODO
+	}
+
+}
